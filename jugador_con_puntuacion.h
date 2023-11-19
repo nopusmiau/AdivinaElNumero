3db3b5556e6679d8b1bbc1ff6800688527a0b2a5
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2023-11-19 16:51:06 -0600

    Add files via upload

---
+++ jugador_con_puntuacion.h
@@ -0,0 +1,17 @@
+#ifndef JUGADOR_CON_PUNTUACION_H
+#define JUGADOR_CON_PUNTUACION_H
+
+#include "jugador.h"
+using namespace std;
+class JugadorConPuntuacion : public Jugador {
+    private:
+        int puntuacion;
+    
+    public:
+        JugadorConPuntuacion(string nombre);
+        int obtenerPuntuacion() const override;
+        int getPuntuacion() const;
+        void setPuntuacion(int nuevaPuntuacion);
+};
+
+#endif
---
+++ manejo_archivos.h
@@ -0,0 +1,17 @@
+#ifndef MANEJO_ARCHIVOS_H
+#define MANEJO_ARCHIVOS_H
+
+#include <string>
+#include "jugador_con_puntuacion.h"
+using namespace std;
+class ManejoArchivos {
+    private: 
+        string nombreArchivo;
+    
+    public:
+        ManejoArchivos(string nombre);
+        void guardarDatos(const JugadorConPuntuacion& jugador);
+        void cargarDatos(JugadorConPuntuacion& jugador);
+};
+
+#endif
