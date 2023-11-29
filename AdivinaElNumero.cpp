e63d92bb1056551e12bc31614d1b4cfb12b1aa08
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2023-11-29 01:04:29 -0600

    Update AdivinaElNumero.cpp

--- AdivinaElNumero.cpp
+++ AdivinaElNumero.cpp
@@ -1,46 +1,73 @@
 #include <iostream>
-#include "JugadorConPuntuacion.h"
-#include "Jugador.h"
-#include "ManejoArchivos.h"
-#include "JuegoDeAdivinanza.h"
-#include "Potenciador.h"
+#include "JugadorConPuntuacion.cpp"
+#include "ManejoArchivos.cpp"
+#include "JuegoDeAdivinanza.cpp"
+#include "Potenciador.cpp"
 using namespace std;
 
 int main(){
-    JugadorConPuntuacion jugador("NombreJugador");
+    string nombreJugador;
+    cout << "Ingrese su nombre: ";
+    std::cin >> nombreJugador;
+    JugadorConPuntuacion jugador(nombreJugador);
     Potenciador potenciador;
     ManejoArchivos manejoArchivos("datos_jugador.txt");
     JuegoDeAdivinanza juego(jugador);
 
+    manejoArchivos.guardarDatos(nombreJugador);
+
     char respuesta;
+    char doblePuntos;
     bool continuar = true;
+    const int puntosPorAdivinanzaExitosa = 10;
 
     while (continuar) {
         cout <<"Bienvenido al juego de adivinanzas" << endl;
-        cout <<"Intenta adivinar el numero secreto entre 1 y 100" << endl;
+        cout <<"Intenta adivinar el numero secreto entre 1 y 25" << endl;
 
         while (!juego.juegoTerminado()) {
             int intento;
             cout << "Introduce tu intento: ";
-            cin >> intento;
+            std::cin >> intento;
 
             juego.adivinar(intento);
 
-            if (juego.ganador()) {
-                cout << "¡Felicidades! Has adivinado el numero secreto" << endl;
-                break;
+            if (intento == juego.obtenerNumeroSecreto()) {
+                cout << "Felicidades. Has adivinado el numero secreto" << endl;
+                jugador.sumarPuntos(puntosPorAdivinanzaExitosa);
+                cout << "¿Quieres doblar los puntos? (s/n)";
+                std::cin >> doblePuntos; 
+                
+                if (doblePuntos != 's' || doblePuntos != 'S'){
+                    cout<< "Jugador: "  << jugador.getNombre() << endl;
+                    cout<< "Puntuacion nueva: "<< jugador.getPuntuacion() << endl;
+                    break;
+                } else {
+                    if (juego.sePuedenUsarPotenciadores()) {
+                        juego.usarPotenciador();
+                        potenciador.utilizarPotenciador(jugador, "DoblePuntuacion");
+                    }
+                    break;
+                }
+
             }
 
             if (juego.juegoTerminado()) {
-                cout << "¡Agotaste tus intentos! El numero secreto era: " << juego.obtenerNumeroSecreto() << endl;
+                cout << "Agotaste tus intentos. El numero secreto era: " << juego.obtenerNumeroSecreto() << endl;
+                cout <<"Jugador: " << jugador.getNombre() << endl;
+                cout <<"Puntuacion actual: " << jugador.getPuntuacion() << endl;
                 break;
             }
+            
+            
         }
 
         cout << "¿Quieres jugar de nuevo? (s/n): ";
-        cin >> respuesta;
+        std::cin >> respuesta;
 
         if (respuesta != 's' && respuesta != 'S'){
+            cout << "Puntuacion final: " << jugador.getPuntuacion() << endl;
+            manejoArchivos.guardarDatos(jugador);
             continuar = false;
         } else {
             juego.reiniciarJuego();
