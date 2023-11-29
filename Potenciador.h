0271ae0d822a9772fd983cc9b97621c95bc9f95f
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2023-11-29 00:56:08 -0600

    Update Potenciador.h

--- Potenciador.h
+++ Potenciador.h
@@ -14,6 +14,10 @@ class Potenciador {
         Potenciador();
         void agregarPotenciador(string potenciador);
         void utilizarPotenciador(JugadorConPuntuacion& jugador, string potenciador);
+        void setPotenciadoresDisponibles(const vector<string>& potenciadores);
+        vector<string> getPotenciadoresDisponibles() const;
+        void setPotenciadoresUtilizados(const vector<string>& potenciadores);
+        vector<string> getPotenciadoresUtilizados() const;
 };
 
 #endif
