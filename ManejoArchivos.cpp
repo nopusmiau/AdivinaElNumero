66c5a7fa3d436b4b6a3e367e6b57284997912254
nopusmiau <150055923+nopusmiau@users.noreply.github.com>
2023-11-29 00:58:41 -0600

    Update ManejoArchivos.cpp

--- ManejoArchivos.cpp
+++ ManejoArchivos.cpp
@@ -1,11 +1,12 @@
 #include "ManejoArchivos.h"
 #include <fstream>
+#include "Jugador.h"
 using namespace std;
 
 ManejoArchivos::ManejoArchivos(string nombre) : nombreArchivo(nombre) {}
 
 void ManejoArchivos::guardarDatos(const JugadorConPuntuacion& jugador) {
-    ofstream archivo(nombreArchivo);
+    ofstream archivo("datos_jugador.txt");
     if (archivo.is_open()) {
         archivo << jugador.getNombre() << "\n";
         archivo << jugador.getPuntuacion() << "\n";
@@ -14,7 +15,7 @@ void ManejoArchivos::guardarDatos(const JugadorConPuntuacion& jugador) {
 }
 
 void ManejoArchivos::cargarDatos(JugadorConPuntuacion& jugador) {
-    ifstream archivo(nombreArchivo);
+    ifstream archivo("datos_jugador.txt");
     if (archivo.is_open()) {
         string nombre;
         int puntuacion;
@@ -23,4 +24,16 @@ void ManejoArchivos::cargarDatos(JugadorConPuntuacion& jugador) {
         jugador.setPuntuacion(puntuacion);
         archivo.close();
     }
+}
+
+void ManejoArchivos::guardarNombre(string nombre){
+    nombreArchivo = nombre;
+}
+
+void ManejoArchivos::setNombreArchivo(const string& nombre) {
+    nombreArchivo = nombre;
+}
+
+string ManejoArchivos::getNombreArchivo() const {
+    return nombreArchivo;
 }
